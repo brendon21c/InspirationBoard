a668bf59fb289253a29c27ab0444cced9d233535
Brendon <brennon.mckeever@gmail.com>
2016-10-18 16:28:46 -0500

    Starting work on Database

---
+++ app/src/main/java/com/brendon/inspirationboard/InspirationDatabase.java
@@ -0,0 +1,32 @@
+package com.brendon.inspirationboard;
+
+/*
+This is the database class.
+ */
+public class InspirationDatabase {
+
+    // DB information
+    protected static final String DB_NAME = "Inspiration";
+    protected static final String DB_TABLE = "inspiration_table";
+    protected static final int DB_VERSION = 1;
+
+    // Column Names
+    protected static final String PRIMARY_KEY_COL = "Primary_Key";
+    protected static final String PICTURE_COL = "Picture";
+    protected static final String HASHTAG_COL = "Picture_Hash";
+    protected static final String NOTES_COL = "Notes";
+    protected static final String DATE_COL = "Date";
+
+    // Tags for error checking.
+    private static final String DB_TAG = "Database Manager";
+    private static final String SQL_HELPER = "SQLHelper";
+
+
+
+
+
+
+
+
+
+}
--- app/src/main/java/com/brendon/inspirationboard/MainActivity.java
+++ app/src/main/java/com/brendon/inspirationboard/MainActivity.java
@@ -3,11 +3,18 @@ package com.brendon.inspirationboard;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.security.Timestamp;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
 }
